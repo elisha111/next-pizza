8eb342e799ce1e6b9315e7776fd0989fa8899173
kovalev.e <kovalev.e@web-canape.ru>
2026-04-03 14:29:46 +0300

    217 скролл по категориям, библиотека react use

--- src/components/shared/products-group-list.tsx
+++ src/components/shared/products-group-list.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { useIntersection } from "react-use";
 import { cn } from "@/lib/utils";
 import { Title } from "./title";
 import { ProductCard } from "./product-card";
+import { useEffect, useRef } from "react";
 
 interface Props {
   title: string;
@@ -12,8 +16,20 @@ interface Props {
 
 export const ProductsGroupList = (props: Props) => {
   const { title, products, className, listClassName, categoryId } = props;
+
+  const intersectionRef = useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  useEffect(() => {
+    if (intersection?.intersectionRatio) {
+      console.log(title, categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting, title]);
+
   return (
-    <div className={className}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
 
       <div className={cn("grid grid-cols-3 gap-12.5", listClassName)}>
